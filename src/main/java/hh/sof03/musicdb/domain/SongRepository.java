81200f1088390db18aa54ef8cb94245a6fc919da
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2023-11-09 10:38:57 +0200

    removed some unused imports

--- src/main/java/hh/sof03/musicdb/domain/SongRepository.java
+++ src/main/java/hh/sof03/musicdb/domain/SongRepository.java
@@ -2,7 +2,6 @@ package hh.sof03.musicdb.domain;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
